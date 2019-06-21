07a8b678e0fd8c3630a7115bc54b7b297e1fae45
Agniel, Denis <dagniel@rand.org>
2019-06-21 11:51:38 -0500

    Updating main sim plot.

--- R/08_main-sim-plots.R
+++ R/08_main-sim-plots.R
@@ -18,30 +18,19 @@ sim_res <- readRDS(here(
 
 res <- sim_res %>%
   filter(
-         # alpha_c == 0.5,
          gamma_n == 0,
          gamma_c == 0,
          lambda_n == 1,
-         lambda_c == 1,
-         # compliance_p == 0.7,
-         # n == 1000,
-         # !shrunk,
-         !(synthetic & theta_0 %in% 
-             c('atregr_est', 'ppregr_est',
-               'ats_est', 'ipw_est',
-               'pps_est', 'ipw_regr_est')),
-         !theta_0 %in% c('ats_est',
-                         'ipw_est',
-                         'pps_est'))
-res <- res %>%
-  mutate(g = paste0(theta_0, shrunk, sample, synthetic, shrunk),
-         synth = case_when(
-           sample == 'CV' & synthetic & !shrunk ~ 'Synthetic-CV',
-           sample == 'full' & synthetic & !shrunk ~ 'Synthetic-full',
-           sample == 'CV' & synthetic  & shrunk ~ 'Synthetic-CV-shrunk',
-           sample == 'full' & synthetic & shrunk ~ 'Synthetic-full-shrunk',
-           TRUE ~ 'Non-synthetic'
-         ))
+         lambda_c == 1) %>%
+  filter((synthetic & theta_0 %in% c('tsls_est') & !shrunk) | 
+           !synthetic & theta_0 %in% c('atregr_est',
+                                       'ipw_regr_est',
+                                       'iv_est',
+                                       'tsls_est'),
+         sample != 'CV') %>%
+  mutate(name = 
+           case_when(synthetic ~ 'SCE',
+                     TRUE ~ theta_0))
 #-----------------------------------------
 ## gamma_c = 0
 ## lambda_n = 1
@@ -52,38 +41,70 @@ res <- res %>%
 #-----------------------------------------------
 ## comparing synthetic vs regular
 #----------------------------------------------
-this_res <- res %>%
-  filter(synth %in% c('Non-synthetic', 'Synthetic-full')) %>%
-  mutate(estimator =
-           case_when(
-             synth == 'non-synthetic' ~ case_when(
-               theta_0 == 'atregr_est' ~ 'AT',
-               theta_0 == 'iv_est' ~ 'IV',
-               theta_0 == 'ppregr_est' ~ 'PP',
-               theta_0 == 'tsls_est' ~ 'Two-stage LS',
-               theta_0 == 'ipw_regr_est' ~ 'PS-model-assisted'
-             ),
-             theta_0 == 'iv_est' ~ 'Synthetic IV',
-             theta_0 == 'tsls_est' ~ 'Synthetic TSLS'
-           ))
-
-mse_pl <- ggplot(this_res,
-                 aes(x = alpha_c, y = mse, group = g, linetype = theta_0, color = synth)) +
-  geom_line() +
-  geom_point() +
+mse_pl <- ggplot(res,
+                  aes(x = alpha_c, y = mse, group = name, 
+                      linetype = name,
+                      shape = name,
+                      color = synthetic)) +
+  geom_point(size = 3) +
+  geom_line() + 
   theme_bw(base_size = 16) +
-  # scale_y_log10() +
-  labs(x = expression(alpha[c]), y = 'MSE') +
-  ggtitle(expression('MSE as a function of NCE violation'),
-          subtitle = 'Simulation 1, by sample size and compliance proportion') +
-  scale_color_grey('', end = 0.75) +
-  scale_linetype_discrete(expression(hat(theta)),
-                          labels = c('As-treated', 
-                                     'PS-model-assisted',
-                                     'IV', 'Per-protocol',
-                                     'Two-stage LS')) +
-  facet_grid(compliance_p ~ n, scales = 'free')
-
+  ggtitle('MSE as a function of NCE violation',
+          subtitle = 'Data-generating mechanism 2 (Stuart and Jo)') +
+  labs(x = expression(alpha[c]), y = 'Bias') +
+  scale_linetype_manual(expression(hat(theta)),
+                        breaks = c('SCE',
+                                   'tsls_est',
+                                   'iv_est',
+                                   'atregr_est',
+                                   'ipw_regr_est'),
+                        labels = c('SCE',
+                                   'Two-stage LS',
+                                   'IV',
+                                   'As-treated', 
+                                   'PS-model-assisted'),
+                        values = c('dotdash',
+                                   'longdash',
+                                   'dotted',
+                                   'solid',
+                                   'twodash')) +
+  scale_shape_discrete(expression(hat(theta)),
+                       breaks = c('SCE',
+                                  'tsls_est',
+                                  'iv_est',
+                                  'atregr_est',
+                                  'ipw_regr_est'),
+                       labels = c('SCE',
+                                  'Two-stage LS',
+                                  'IV',
+                                  'As-treated', 
+                                  'PS-model-assisted')) +
+  facet_grid(compliance_p ~ n, scales = 'free') +
+  scale_color_manual('',
+                     breaks = c(FALSE, TRUE),
+                     labels = c('Candidate', 'Synthetic'),
+                     values = c('#fdbb84','#252525')
+  ) +
+  theme(legend.position = 'bottom', legend.key.size = unit(1.5, 'cm')) +
+  scale_y_log10()
 mse_pl
+# mse_pl <- ggplot(this_res,
+#                  aes(x = alpha_c, y = mse, group = g, linetype = theta_0, color = synth)) +
+#   geom_line() +
+#   geom_point() +
+#   theme_bw(base_size = 16) +
+#   # scale_y_log10() +
+#   labs(x = expression(alpha[c]), y = 'MSE') +
+#   ggtitle(expression('MSE as a function of NCE violation'),
+#           subtitle = 'Simulation 1, by sample size and compliance proportion') +
+#   scale_color_grey('', end = 0.75) +
+#   scale_linetype_discrete(expression(hat(theta)),
+#                           labels = c('As-treated', 
+#                                      'PS-model-assisted',
+#                                      'IV', 'Per-protocol',
+#                                      'Two-stage LS')) +
+#   facet_grid(compliance_p ~ n, scales = 'free')
+# 
+# mse_pl
 ggsave(here('figures/main-sim-mse-plot.png'),
        height = 16, width = 16)
